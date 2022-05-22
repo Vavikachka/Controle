048d85497d8241ce51ac0efba3ec8ea6304d06da
Vika <vavikachka48gmail.com>
2022-05-22 17:41:15 +0300

    переделанная программа

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
-﻿int count=3;
-string[] Array1= {"3","Vika","))","90","File","78","City"};
-string [] Array2= new string [count];
-Console.Write("{");
-ChangesArray(Array2,Array1);
-Console.Write("}");
+﻿string[] Array1= {"35","Vika","))","97","File","7","City"};
+int count=Array1.Length;
+string [] Array2=new string [count];
+ChangesArray(Array1,Array2);
 
-void ChangesArray(string [] arr2,string  [] arr1){
-    for(int i=0; i<arr2.Length;i++){
-        arr2[i]=arr1[i];
-        Console.Write($"{arr2[i]} ");
+void ChangesArray(string  [] arr1,string [] arr2){
+    for(int i=0; i<arr1.Length;i++){
+        int len=arr1[i].Length;
+        if(len<=3){ 
+         int i1=0;
+         arr2[i1]=arr1[i];
+         Console.Write($"{arr2[i1]} ");
+         i1++;
+        }
     }
 }
+
+
